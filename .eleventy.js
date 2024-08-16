26c83599252241351b52c1cd1e6f3b99d697daf6
Matt <matt@b10345gh.vetsci.usyd.edu.au>
2024-08-16 20:52:01 +1000

    font and code

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = config => {
   config.addCollection('post', collection => {
     return collection
       .getFilteredByGlob('./src/posts/*.md')
       .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
   });
+  config.addPlugin(syntaxHighlight);
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
--- src/posts/anki.md
+++ src/posts/anki.md
@@ -5,3 +5,9 @@ displayOrder: 2
 ---
 
 This is my article on the benefits of spaced repition.
+
+```clike
+void main(){
+    printf("test post please ignore\n");
+}
+```
