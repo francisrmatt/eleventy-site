07e6a059f812312c2ae6999de1368740ae8da26d
Matt <matt@Matts-Macbook-Pro.local>
2024-08-27 14:58:39 +1000

    testing api

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,17 +1,21 @@
-export async function onRequestPost(request) {
-
-    const PRESHARED_AUTH_HEADER_KEY = "pw";
-    const PRESHARED_AUTH_HEADER_VALUE = "secret_pw";
-    const psk = request.headers.get(PRESHARED_AUTH_HEADER_KEY);
-
-    if (psk === PRESHARED_AUTH_HEADER_VALUE) {
-      // Correct preshared header key supplied. Fetch request from origin.
-      //return fetch(request);
-      return new Response('well done');
-    }
-
-    // Incorrect key supplied. Reject the request.
-    return new Response("Sorry, you have supplied an invalid key.", {
-      status: 403,
-    }); 
-}
+export default {
+    async fetch(request) {
+        /**
+         * @param {string} PRESHARED_AUTH_HEADER_KEY Custom header to check for key
+         * @param {string} PRESHARED_AUTH_HEADER_VALUE Hard coded key value
+         */
+        const PRESHARED_AUTH_HEADER_KEY = "pw";
+        const PRESHARED_AUTH_HEADER_VALUE = "secret_pw";
+        const psk = request.headers.get(PRESHARED_AUTH_HEADER_KEY);
+    
+        if (psk === PRESHARED_AUTH_HEADER_VALUE) {
+          // Correct preshared header key supplied. Fetch request from origin.
+          return new Response('noice')
+        }
+    
+        // Incorrect key supplied. Reject the request.
+        return new Response("Sorry, you have supplied an invalid key.", {
+          status: 403,
+        });
+      },
+  };
